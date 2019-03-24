7bc8d75543aa70ce4ab0092533dac3777a2f0b78
alamin <alamin785as@gmail.com>
2019-03-23 21:38:48 -0700

    first commit

--- fireofire/ViewController.swift
+++ fireofire/ViewController.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    var list:[[String:AnyObject]] = [[String:AnyObject]]()
+    
+    @IBOutlet weak var tabl: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+       response()
+        
+        
+        
+    
     }
-
-
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "fire") as! UITableViewCell
+        let item = self.list [indexPath.row]
+        cell.textLabel?.text = item["capital"] as? String
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 108
+    }
+    
+    
+    func response() {
+        
+        AF.request("https://restcountries.eu/rest/v2/all").responseJSON { response in
+            switch response.result{
+            case.success:
+                self.list = response.result.value as! [[String:AnyObject]]
+                self.tabl.reloadData()
+            case.failure(let error):
+                print(error)
+                
+            }
+        }
+        
+    }
 }
 
